c3bb94f636e38298efbbe50a432fa3ebd74e80e6
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-07-12 23:28:59 +0100

    basic clock with current time

---
+++ digitalclock.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+int get_current_time() {
+    time_t t, val = 1;
+    struct tm* current_time;
+
+    t = time(NULL);
+    current_time = localtime(&t);
+    printf("%d:%d:%d\n", current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
+
+    return current_time;
+}
